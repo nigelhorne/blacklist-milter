0752934bf5c9b2b2be3c5d9cb7c141f3575224b0
Nigel Horne <njh@bandsman.co.uk>
2017-07-13 14:24:34 +0100

    Added the systemd file

--- blacklist-milter.c
+++ blacklist-milter.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2006-2016 Nigel Horne <njh@bandsman.co.uk>
+ *  Copyright (C) 2006-2017 Nigel Horne <njh@bandsman.co.uk>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
